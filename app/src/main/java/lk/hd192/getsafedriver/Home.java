6bf76e92a74d404053213c7906f7386f5608849e
Udeesha Bandara <udeeshabandara@gmail.com>
2020-12-28 00:09:34 +0530

    Home dashboard implemented

---
+++ app/src/main/java/lk/hd192/getsafedriver/Home.java
@@ -0,0 +1,152 @@
+package lk.hd192.getsafedriver;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
+
+public class Home extends AppCompatActivity {
+
+    RecyclerView recyclerHome;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.yellow));
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+
+        recyclerHome = findViewById(R.id.recycler_home);
+
+        recyclerHome.setAdapter(new HomeItemAdapter());
+        recyclerHome.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+//        staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+//        staggeredGridLayoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
+//
+//        recyclerHome.setLayoutManager(staggeredGridLayoutManager);
+//        recyclerHome.getAdapter().notifyDataSetChanged();
+
+
+    }
+
+    class LocationViewHolder extends RecyclerView.ViewHolder {
+
+        public LocationViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    class AbsenceViewHolder extends RecyclerView.ViewHolder {
+
+        public AbsenceViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    class PaymentViewHolder extends RecyclerView.ViewHolder {
+
+        public PaymentViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    class AttendanceViewHolder extends RecyclerView.ViewHolder {
+
+        public AttendanceViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    class HomeItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View locationView = LayoutInflater.from(getApplicationContext())
+                    .inflate(R.layout.item_location, parent, false);
+            View messageView = LayoutInflater.from(getApplicationContext())
+                    .inflate(R.layout.item_message, parent, false);
+            View absenceView = LayoutInflater.from(getApplicationContext())
+                    .inflate(R.layout.item_absence, parent, false);
+            View paymentView = LayoutInflater.from(getApplicationContext())
+                    .inflate(R.layout.item_payment, parent, false);
+            View attendanceView = LayoutInflater.from(getApplicationContext())
+                    .inflate(R.layout.item_attendance, parent, false);
+            switch (viewType) {
+
+                case 1:
+                    return new LocationViewHolder(locationView);
+                case 3:
+                    return new MessageViewHolder(messageView);
+                case 0:
+                    return new AbsenceViewHolder(absenceView);
+                case 2:
+                    return new PaymentViewHolder(paymentView);
+                case 4:
+                    return new AttendanceViewHolder(attendanceView);
+
+            }
+            return null;
+
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            if (position == 0)
+                return 0;
+            else if (position == 1)
+                return 1;
+            else if (position == 2)
+                return 2;
+            else if (position == 3)
+                return 3;
+            else
+                return 4;
+
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+            try {
+                switch (holder.getItemViewType()) {
+
+                    case 0:
+
+                        break;
+                    case 1:
+
+                        break;
+                }
+
+
+            } catch (Exception e) {
+
+            }
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
+    }
+}
